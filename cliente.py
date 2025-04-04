11209f1eac0c73fb17ed4d1978b6d88db283f027
joao <jmarcelopq@gmail.com>
2025-04-04 17:20:48 -0300

    Ajeitei para ser apropriado a entrega 1

--- cliente.py
+++ cliente.py
@@ -8,14 +8,44 @@ address = (host, port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(address)
 
+tamanho_maximo = 2
+tamanho_operacao = 3
+modo_operacao = "GBN"
+selecionado = 0
 
-tamanho_maximo = "1024"
-modo_operacao = "padrao"
-mensagem_handshake = f"HANDSHAKE|{modo_operacao}|{tamanho_maximo}"
+while(selecionado == 0):
+    modo_operacao = input("Qual será o modo de operação?: ")
+    if(modo_operacao.upper() == "GO-BACK-N" or "GOBACKN" or "BACKN" or "BACK-N" or "GBN" or "GB" or "BN" or "G"):
+        modo_operacao = "GBN"
+        selecionado = 1
+        break
+    elif(modo_operacao.upper() == "REPETIÇÃO" or "REPETION" or "REPETIÇÃO SELETIVA" or "SELECTIVE REPEAT" or "REPETE" or "REPEAT" or "SR" or "S" or "R"):
+        modo_operacao = "SR"
+        selecionado = 1
+        break
+    else:
+        print("Isto não é um modo válido! Por favor tente novamente")
+
+selecionado = 0
+tamanho_operacao = int(input(f"Qual será o tamanho da mensagem? (Uma caractér será reservada para o número do tamanho em sí) (Máximo: {tamanho_maximo})  "))
+
+while(selecionado == 0):
+    if(tamanho_operacao > tamanho_maximo):
+        print(f"Tamanho máximo excedido! Por favor retorne um tamanho válido")
+    elif(tamanho_operacao <= 0):
+        print(f"Tamanho não pode ser menor que ou igual a 0! Por favor retorne um tamanho válido")
+    else:
+        selecionado = 1
+        break
+    tamanho_operacao = int(input(f"Qual será o tamanho da mensagem? (Máximo: {tamanho_maximo})  "))
+
+if(len(modo_operacao) > tamanho_operacao):
+    modo_operacao = modo_operacao[0:tamanho_operacao]
+
+mensagem_handshake = f"{modo_operacao}{tamanho_operacao+1}"
 sock.send(mensagem_handshake.encode())
 
-
-resposta = sock.recv(1024).decode()
+resposta = sock.recv(tamanho_maximo+1).decode()
 print("Resposta do servidor:", resposta)
 
 sock.close()
--- servidor.py
+++ servidor.py
@@ -8,15 +8,23 @@ server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(address)
 server_sock.listen(1)
 
+tamanho_maximo = 3
+resposta = "OK"
+
 print("Aguardando conexão...")
 conn, client_addr = server_sock.accept()
 print(f"Conectado a {client_addr}")
 
-
-mensagem = conn.recv(1024).decode()
+mensagem = conn.recv(tamanho_maximo).decode()
 print("Mensagem recebida:", mensagem)
 
-conn.send("HANDSHAKE_OK".encode())
+if(mensagem[-1].isdigit() == True):
+    tamanho_maximo = int(mensagem[-1])
+
+if(len(resposta) > tamanho_maximo):
+    resposta = resposta[tamanho_maximo-1]
+
+conn.send(resposta.encode())
 
 conn.close()
 server_sock.close()
